749d2a5f8dd3123e7b70d42bedfca319f712c985
Magd Aref <69316030+Magd74NA@users.noreply.github.com>
2026-02-25 15:53:45 -0500

    refactor: rename module and restructure into subpackage
    Renames the module from `jdextractor` to `jdextract`. Moves the application logic into a new `jdextract` sub-package and updates the import paths in `main.go`.

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"jdextractor"
+	"jdextract/jdextract"
 )
 
 func main() {
 	fmt.Println("Hello World!")
-	jdextractor.CheckWiring()
+	jdextract.CheckWiring()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module jdextractor
+module jdextract
 
 go 1.25.0
