552b65f34332a09fcfbf81c39fe7bc21b9cbfc81
Magd Aref <69316030+Magd74NA@users.noreply.github.com>
2026-02-26 00:57:46 -0500

    build(deps): add toon-go library for TOON format support
    Add github.com/toon-format/toon-go as a dependency with vendored
    files. The library is imported in the parse module to enable
    TOON format parsing capabilities.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module jdextract
 
 go 1.25.0
+
+require github.com/toon-format/toon-go v0.0.0-20251202084852-7ca0e27c4e8c
--- jdextract/parse.go
+++ jdextract/parse.go
@@ -3,6 +3,8 @@ package jdextract
 import (
 	"regexp"
 	"strings"
+
+	_ "github.com/toon-format/toon-go"
 )
 
 // NodeType constants, ordered from most generic to most specific.
