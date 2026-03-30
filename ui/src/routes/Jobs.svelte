0543492e48cbd4b29fdc601d204d49260ddc41f0
Magd Aref <69316030+Magd74NA@users.noreply.github.com>
2026-03-30 16:15:06 -0400

    refactor(ui): remove 'Process New' button and unused import
    Removes the "Process New" button and the associated `push` import from svelte-spa-router in the Jobs view. Also normalizes import quotes.

--- ui/src/routes/Jobs.svelte
+++ ui/src/routes/Jobs.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-  import { push } from 'svelte-spa-router';
-  import JobsTable from '../components/JobsTable.svelte';
+  import JobsTable from "../components/JobsTable.svelte";
 </script>
 
 <div class="page-header">
   <h2>Applications</h2>
-  <button onclick={() => push('/process')}>+ Process New</button>
 </div>
 
 <JobsTable />
